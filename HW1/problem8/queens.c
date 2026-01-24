e6115c14797d65aba8d921981188fb0f6d5d742d
Panayiotis Charalambous <panayiotis@cyprus.com>
2026-01-24 10:30:56 +0100

    safe added

--- HW1/problem8/queens.c
+++ HW1/problem8/queens.c
@@ -46,6 +46,22 @@ void solve(int col, int **board){
 
 }
 
+int safe(int row, int col, int **board){
+    // Check this row on left side
+    for(int j=col; j>0; j--){
+        if(board[row][j] == 1) return 0;
+    }
+    // Check upper diagonal on left side
+    for(int i=row, j=col; i>=0 && j>=0; i--, j--){
+        if(board[i][j]==1) return 0;
+    }
+    // Check lower diagonal on left side
+    for(int i=row, j=col; i<N && j>=0; i++, j--){
+        if(board[i][j]==1) return 0;
+    }
+    return 1;
+}
+
 
 int main(){
     int **b = initboard();
