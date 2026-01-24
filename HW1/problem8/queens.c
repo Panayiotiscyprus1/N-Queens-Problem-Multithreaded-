f7f50a7557ba0834f6e4a7a6798137035b561597
Marios Stavrou <marios.stavrou04@gmail.com>
2026-01-24 10:27:37 +0100

    print

--- HW1/problem8/queens.c
+++ HW1/problem8/queens.c
@@ -1,17 +1,41 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <stdio.h>
 
 #define N 8
+
+int** initboard() {
+    int **board = malloc(8 * sizeof(int*));
+
+    for (int i = 0; i < 8; i++) {
+        board[i] = malloc(8 * sizeof(int));
+        for (int j = 0; j < 8; j++) {
+            board[i][j] = 0;  // initialize values
+        }
+    }
+    return board;
+}
+
+void printboard(int **board){
+    for (int i = 0; i < 8; i++) {
+        printf("| ");
+        for (int j = 0; j < 8; j++) {
+            printf("%i | ", board[i][j]);
+        }
+        printf("\n|---|---|---|---|---|---|---|---|\n");
+    }
+}
+
 
 void solve(int col, int **board){
 
     if(col == N){
-        printboard();
+        printboard(board);
         return;
     }
 
     for(int row = 0; row < N; row++){
-        if safe(row, col, board){
+        if (safe(row, col, board)){
             board[row][col] = 1;
 
             solve(col+1, board);
@@ -22,6 +46,8 @@ void solve(int col, int **board){
 
 }
 
-int main(){
 
+int main(){
+    int **b = initboard();
+    printboard(b);
 }
