c2fda817479ed58f0ecaabeff39c3e97ba039922
Panayiotis Charalambous <panayiotis@cyprus.com>
2026-01-24 11:30:20 +0100

    pthead try 11 (seg fault)

--- HW1/problem8/queens.c
+++ HW1/problem8/queens.c
@@ -1,34 +1,69 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "board.h"
 #include "safe.h"
 
 #define N 8
 
-int count = 0;
-void solve(int col, int **board){
+int results[N];
+int total = 0;
+
+typedef struct {
+    int col;
+    int row;
+    int **board;
+} solveArg;
+
+
+void *solve(void *arg){
+
+    solveArg *args = (solveArg *)arg; 
+    int col = args->col;
+    int row = args->row;
+    int **board = args->board;
+    int id = row;
 
     if(col == N){
-        count++;
-        printf("count: %i \n", count);
+        results[id]+=1;
+        printf("I am thread %i with count %i \n", id, results[id]);
         printboard(board);
-        return;
+        return(NULL);
     }
 
-    for(int row = 0; row < N; row++){
-        if (safe(row, col, board)){
-            board[row][col] = 1;
-
-            solve(col+1, board);
+    for(int r = row; r < N; r++){
+        if (safe(r, col, board)){
+            board[r][col] = 1;
             
-            board[row][col] = 0;
+            args->row = args->row + 1;
+            args->board = board;
+
+            solve(args);
+            board[r][col] = 0;
         }
     }
-
+    return(NULL);
 }
 
 int main(){
-    int **b = initboard();
-    solve(0, b);
-    printf("%i", count);
+    solveArg **arg = malloc(N*sizeof(*arg));
+
+    for(int i=0; i<N; i++){
+        arg[i]->col=0;
+        arg[i]->row=i;
+        arg[i]->board=initboard();
+    }
+
+
+    pthread_t tid[N];
+    for(long i=0; i<N; i++){ 
+        pthread_create(&tid[i], NULL, (void *)solve, arg[i]); 
+    }
+
+    for(int i=0; i<N; i++){
+        pthread_join(tid[i], NULL);
+        total += results[i];
+    }
+
+    printf("%i", total);
 }
