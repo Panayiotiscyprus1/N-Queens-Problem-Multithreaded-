77dd8c4ff3c4e9ea4e4fd2f639ad630139b2cb89
Marios Stavrou <marios.stavrou04@gmail.com>
2026-01-27 12:26:53 +0100

    hm

--- HW2/problem4/queens_omp.c
+++ HW2/problem4/queens_omp.c
@@ -8,35 +8,38 @@
 #define N 8
 
 int count = 0;
+
 void solve(int col, int **board){
-    #pragma omp parallel
-    {
-        if(col == N){
-            #pragma omp critical
-            {
-                count++;
-                printf("\ncount: %i \n", count);
-                printboard(board);
-            }
+    
+    if(col == N){
+        #pragma omp critical
+        {
+            count++;
+            printf("\ncount: %i \n", count);
+            printboard(board);
             return;
         }
-
-        #pragma omp parallel for
-        for(int row = 0; row < N; row++){
-            if (safe(row, col, board)){
-                board[row][col] = 1;
-
-                solve(col+1, board);
-                
-                board[row][col] = 0;
-            }
-        }
+        
     }
 
+    for(int row = 0; row < N; row++){
+        if (safe(row, col, board)){
+            board[row][col] = 1;
+
+            solve(col+1, board);
+            
+            board[row][col] = 0;
+        }
+    }
 }
 
 int main(){
-    int **b = initboard();
-    solve(0, b);
+    #pragma omp parallel for reduction (+:count)
+    for(int row = 0; row < N; row++){
+        int **local_b = initboard();
+        local_b[row][0] = 1;
+        solve(1, local_b);
+    }
+    
     printf("total count: %i\n", count);
 }
